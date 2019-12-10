286faf898f215a8f43d19a3af1dde603280342ea
Nick M <58752614+nickmartin1ee7@users.noreply.github.com>
2019-12-10 18:01:37 -0500

    Added main project files

---
+++ MaxicodeDecoder/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace MaxicodeDecoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.maxicodeTextBox = new System.Windows.Forms.TextBox();
+            this.trackingNumTextBox = new System.Windows.Forms.TextBox();
+            this.trackingNumLabel = new System.Windows.Forms.Label();
+            this.infoButton = new System.Windows.Forms.Button();
+            this.maxicodeLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.maxicodePictureBox = new System.Windows.Forms.PictureBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.maxicodePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // maxicodeTextBox
+            // 
+            this.maxicodeTextBox.Location = new System.Drawing.Point(124, 27);
+            this.maxicodeTextBox.Name = "maxicodeTextBox";
+            this.maxicodeTextBox.Size = new System.Drawing.Size(418, 20);
+            this.maxicodeTextBox.TabIndex = 0;
+            this.maxicodeTextBox.TextChanged += new System.EventHandler(this.maxicodeTextBox_TextChanged);
+            // 
+            // trackingNumTextBox
+            // 
+            this.trackingNumTextBox.Location = new System.Drawing.Point(124, 75);
+            this.trackingNumTextBox.Name = "trackingNumTextBox";
+            this.trackingNumTextBox.ReadOnly = true;
+            this.trackingNumTextBox.Size = new System.Drawing.Size(418, 20);
+            this.trackingNumTextBox.TabIndex = 2;
+            // 
+            // trackingNumLabel
+            // 
+            this.trackingNumLabel.AutoSize = true;
+            this.trackingNumLabel.Location = new System.Drawing.Point(12, 78);
+            this.trackingNumLabel.Name = "trackingNumLabel";
+            this.trackingNumLabel.Size = new System.Drawing.Size(106, 13);
+            this.trackingNumLabel.TabIndex = 5;
+            this.trackingNumLabel.Text = "1Z Tracking number:";
+            // 
+            // infoButton
+            // 
+            this.infoButton.Enabled = false;
+            this.infoButton.Location = new System.Drawing.Point(124, 108);
+            this.infoButton.Name = "infoButton";
+            this.infoButton.Size = new System.Drawing.Size(74, 29);
+            this.infoButton.TabIndex = 3;
+            this.infoButton.Text = "Info";
+            this.infoButton.UseVisualStyleBackColor = true;
+            this.infoButton.Click += new System.EventHandler(this.infoButton_Click);
+            // 
+            // maxicodeLinkLabel
+            // 
+            this.maxicodeLinkLabel.AutoSize = true;
+            this.maxicodeLinkLabel.LinkColor = System.Drawing.Color.Navy;
+            this.maxicodeLinkLabel.Location = new System.Drawing.Point(62, 27);
+            this.maxicodeLinkLabel.Name = "maxicodeLinkLabel";
+            this.maxicodeLinkLabel.Size = new System.Drawing.Size(56, 13);
+            this.maxicodeLinkLabel.TabIndex = 6;
+            this.maxicodeLinkLabel.TabStop = true;
+            this.maxicodeLinkLabel.Text = "Maxicode:";
+            this.maxicodeLinkLabel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.maxicodeLinkLabel_MouseClick);
+            this.maxicodeLinkLabel.MouseLeave += new System.EventHandler(this.maxicodeLinkLabel_MouseLeave);
+            // 
+            // maxicodePictureBox
+            // 
+            this.maxicodePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("maxicodePictureBox.Image")));
+            this.maxicodePictureBox.Location = new System.Drawing.Point(124, 12);
+            this.maxicodePictureBox.Name = "maxicodePictureBox";
+            this.maxicodePictureBox.Size = new System.Drawing.Size(100, 100);
+            this.maxicodePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.maxicodePictureBox.TabIndex = 7;
+            this.maxicodePictureBox.TabStop = false;
+            this.maxicodePictureBox.Visible = false;
+            // 
+            // clearButton
+            // 
+            this.clearButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.clearButton.Location = new System.Drawing.Point(468, 108);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(74, 29);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.infoButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.clearButton;
+            this.ClientSize = new System.Drawing.Size(561, 149);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.maxicodePictureBox);
+            this.Controls.Add(this.maxicodeLinkLabel);
+            this.Controls.Add(this.infoButton);
+            this.Controls.Add(this.trackingNumTextBox);
+            this.Controls.Add(this.trackingNumLabel);
+            this.Controls.Add(this.maxicodeTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Maxicode Decoder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            ((System.ComponentModel.ISupportInitialize)(this.maxicodePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox maxicodeTextBox;
+        private System.Windows.Forms.TextBox trackingNumTextBox;
+        private System.Windows.Forms.Label trackingNumLabel;
+        private System.Windows.Forms.Button infoButton;
+        private System.Windows.Forms.LinkLabel maxicodeLinkLabel;
+        private System.Windows.Forms.PictureBox maxicodePictureBox;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ MaxicodeDecoder/Form1.cs
@@ -0,0 +1,187 @@
+﻿/**
+ *  Author: Nicholas Martin
+ *  Date:   12/10/2019
+ *  Description:    Parses Maxicode into a 1Z tracking code and displays additional information
+ **/
+
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using CsvHelper;
+
+namespace MaxicodeDecoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Global variables
+        // ________________
+        // Minimum string length for maxicode
+        const int MIN_LENGTH = 42;
+
+        // UPS header of maxicode format
+        const string UPS_HEADER = "[)>0196";
+
+        // Beginning of tracking code
+        const string POS_1 = "1Z";
+
+        // Midpoint of tracking code
+        const string POS_2 = "UPSN";
+
+        // US country code
+        const string US_COUNTRY_CODE = "840";
+
+        bool valid = false;
+        DataTable zipTable = new DataTable();
+        string maxicode;
+
+        // Debug autofill
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            maxicodeTextBox.Text = "[)>01963360910628400021Z14647438UPSN410E1W1951/1Y123RoadTAMPAFL";   // Sample Maxicode
+        }
+
+        // TextBox text change: update result textbox
+        private void maxicodeTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if ((maxicodeTextBox.Text.Length >= MIN_LENGTH && maxicodeTextBox.Text.StartsWith(UPS_HEADER) && maxicodeTextBox.Text.Contains(POS_1) && maxicodeTextBox.Text.Contains(POS_2)))
+            {
+                maxicode = maxicodeTextBox.Text;
+                trackingCodeParser(maxicode, out string trackingCode);
+                valid = true;
+                trackingNumTextBox.Text = trackingCode;
+                infoButton.Enabled = true;
+            }
+            else
+            {
+                valid = false;
+                trackingNumTextBox.Text = "";
+                infoButton.Enabled = false;
+            }
+        }
+
+        // Button click: show details for tracking number
+        private void infoButton_Click(object sender, EventArgs e)
+        {
+            if (valid)
+            {
+                addDetails(maxicode, out string location, out string serviceCode, out string shipperNum, out string zipCode, out string city, out string state);
+                MessageBox.Show($"Label type:\t{location}\n" +
+                    $"Service code:\t{serviceCode}\nShipper #:\t{shipperNum}\n" +
+                    $"Zipcode:\t\t{zipCode}\n" +
+                    $"City:\t\t{city}\n" +
+                    $"State:\t\t{state}",
+                    "Additional information",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        //Additional detail method
+        private void addDetails(string maxicode, out string location, out string serviceCode, out string shipperNum, out string zipCode, out string city, out string state)
+        {
+            IsDomestic(maxicode, out bool domestic);
+            // Country
+            if (domestic)
+            {
+                location = "Domestic";
+            }
+            else
+            {
+                location = "International";
+            }
+            serviceCode = maxicode.Substring(maxicode.IndexOf(POS_1) - 2, 2);
+            shipperNum = maxicode.Substring(maxicode.IndexOf(POS_2) + 4, 6);
+            // Zipcode
+            if (domestic)
+            {
+                zipCode = maxicode.Substring(7, 5);
+                queryZip(zipTable, zipCode, out city, out state);
+            }
+            else
+            {
+                zipCode = "N/A";
+                city = "N/A";
+                state = "N/A";
+            }
+        }
+
+        // Parse the tracking code
+        private void trackingCodeParser(string maxicode, out string trackingCode) // Out strings
+        {
+            try
+            {
+                trackingCode = maxicode.Substring(maxicode.IndexOf(POS_1), 2) + maxicode.Substring(maxicode.IndexOf(POS_2) + 4, 6) + maxicode.Substring(maxicode.IndexOf(POS_1) - 2, 2) + maxicode.Substring(maxicode.IndexOf(POS_1) + 2, 8);
+
+            }
+            catch (Exception)
+            {
+                trackingCode = "";
+            }
+        }
+
+        //Queries database for city and state
+        private void queryZip(DataTable dataTable, string zipCode, out string city, out string state)
+        {
+            city = $"{zipTable.Rows.Find(zipCode).Field<string>("city")}";
+            state = $"{zipTable.Rows.Find(zipCode).Field<string>("state_name")}";
+        }
+
+        // Determines if tracking code is domestic
+        private void IsDomestic(string maxicode, out bool domestic)
+        {
+            if (maxicode.Contains(US_COUNTRY_CODE))
+            {
+                domestic = true;
+            }
+            else
+            {
+                domestic = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Load resources
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith("usZips.csv"));
+            Console.WriteLine("Trying to load resource: " + resourceName);
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    using (CsvReader csvReader = new CsvReader(reader))
+                    {
+                        {
+                            //Console.WriteLine("Printing resources below:");
+                            //foreach (string s in assembly.GetManifestResourceNames())
+                            //  System.Diagnostics.Debug.WriteLine(s);
+                            using (CsvDataReader csvDataReader = new CsvDataReader(csvReader))
+                            {
+                                zipTable.Load(csvDataReader);
+                                zipTable.PrimaryKey = new DataColumn[] { zipTable.Columns["zip"] };
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void maxicodeLinkLabel_MouseClick(object sender, MouseEventArgs e) => maxicodePictureBox.Visible = true;
+
+        private void maxicodeLinkLabel_MouseLeave(object sender, EventArgs e) => maxicodePictureBox.Visible = false;
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            maxicodeTextBox.Text = "";
+            maxicodeTextBox.Select();
+        }
+    }
+}
